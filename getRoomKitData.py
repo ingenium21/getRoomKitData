42f964a5cba874c33a8c08f6c33c5cc22d10078b
Renato Regalado <renato.regalado@gmail.com>
2021-12-07 14:48:55 -0600

    separated the commands

--- getRoomKitData.py
+++ getRoomKitData.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def start_connect():
-    """Connects to the host via ssh"""
+    """Connects to the host via ssh and outputs a channel object"""
     host = os.getenv("HOST")
     port = os.getenv("PORT")
     username = os.getenv("USERNAME")
@@ -17,21 +17,24 @@ def start_connect():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, int(port), username, password)
+    return ssh
+
+def send_command(ssh, command = "xstatus\r"):
     channel = ssh.invoke_shell()
-    channel.send("xstatus\r")
+    channel.send(command)
     while not channel.recv_ready():
         time.sleep(3)
     out = channel.recv(9999)
     string = out.decode('ascii')
     channel.close()
-    ssh.close()
     return string
 
 
-def data_to_dict(data):
+def data_to_dict(data, pattern='*s '):
     """converts the data you give it into a dictionary"""
-    data = data.split('*s ')
-    data.pop(0)
+    data = data.split(pattern)
+    if pattern not in data[0]:
+        data.pop(0)
     del data[-1]
     my_dict = {}
     for item in data:
@@ -40,17 +43,27 @@ def data_to_dict(data):
     return my_dict
 
 
-def dict_to_json(myDict):
+def dict_to_json(myDict, command, device):
     """spits out dict into a json file"""
+    filename = f"/{device}_{command}_output.json"
     filepath = os.getenv("JSON_PATH")
+    if os.path.exists(filepath) == False:
+        os.mkdir(filepath)
+    filename = f"{filepath}{filename}"
     with open(filepath, 'w') as fp:
         json.dump(myDict, fp)
 
-def main():
-    data = start_connect()
-    mydict = data_to_dict(data)
-    dict_to_json(mydict)
+def session_close(ssh):
+    ssh.close()
 
+def main():
+    device = "swiney lab"
+    command = "xstatus\r"
+    session = start_connect()
+    data = send_command(session, command)
+    mydict = data_to_dict(data)
+    dict_to_json(mydict, command, device)
+    session_close()
 
 if __name__ == "__main__":
     main()
