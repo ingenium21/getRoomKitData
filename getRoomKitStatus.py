0d7e6b9e3d2b90c09e065faf28993bb63058c64e
Renato Regalado <renato.regalado@gmail.com>
2021-12-13 15:20:16 -0600

    Create getRoomKitStatus.py

---
+++ getRoomKitStatus.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+from getRoomKitData import send_command, data_to_dict, dict_to_json, start_connect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_xstatus():
+    command = "xStatus\r"
+    commandTrimmed = "xStatus"
+    data = send_command(session, command)
+    # with open('./callHistory.txt', 'r') as ch:
+    #     data = ch.read()    callsArray.pop(0)
+    mydict = data_to_dict(data, pattern="*s ")
+    dict_to_json(mydict, commandTrimmed, device=device)
+
+def main():
+    device = os.getenv("DEVICE")
+    session = start_connect()
+    get_xstatus(session)
+
+if __name__ == "__main__":
+    main()
